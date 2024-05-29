cbc9e7a0665bb0b96629bd383b2d23b1d7e469d8
DatPham1103 <phamtdat1103@gmail.com>
2024-05-29 12:10:57 +0700

    add login

--- src/layouts/LoginLayout/LoginLayout.tsx
+++ src/layouts/LoginLayout/LoginLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Footer from 'src/components/Footer'
-import LoginHeader from 'src/components/LoginHeader'
+// import Footer from 'src/components/Footer'
+// import LoginHeader from 'src/components/LoginHeader'
 
 interface Props {
   children?: React.ReactNode
@@ -9,9 +9,9 @@ interface Props {
 export default function LoginLayout({ children }: Props) {
   return (
     <div>
-      <LoginHeader />
+
       {children}
-      <Footer />
+
     </div>
   )
 }

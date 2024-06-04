ca35ecabffeed34e0bc1ab695da574b75e48438a
DatPham1103 <phamtdat1103@gmail.com>
2024-06-04 20:07:34 +0700

    Fix/UI

--- src/pages/Approval/Approval.tsx
+++ src/pages/Approval/Approval.tsx
@@ -4,7 +4,7 @@ import ApprovalItem from 'src/pages/Approval/ApprovalItem/ApprovalItem'
 export default function Approval() {
   return (
     <div className='grid grid-cols-10 gap-4 h-full'>
-      <div className='col-span-4 border border-slate-300 rounded-md p-4 bg-white'>
+      <div className='col-span-4 border border-slate-300 rounded-md p-3 bg-white'>
         <div className='p-3 mb-4 border border-slate-300 shadow-lg rounded-md font-medium text-md text-gray-500 grid grid-cols-8'>
           <div className='col-span-1 border-r-2'>No</div>
           <div className='col-span-4 border-r-2 pl-3'>Employee Name</div>
@@ -21,6 +21,33 @@ export default function Approval() {
         <ApprovalItem number='09' name='Trần Thị Gâu Gâu' createAt='24:00 24-12-2004' />
         <ApprovalItem number='10' name='Trần Thị Gâu Gâu' createAt='24:00 24-12-2004' />
         <ApprovalItem number='11' name='Trần Thị Gâu Gâu' createAt='24:00 24-12-2004' />
+
+        <div className='flex justify-center mt-1'>
+        <div className='bg-slate-300/90 px-5 rounded-md mr-1 flex items-center transition'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='white'
+            className='size-7'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18' />
+          </svg>
+        </div>
+        <div className='bg-slate-300/90 px-5 rounded-md ml-1 flex items-center transition'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='white'
+            className='size-7'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3' />
+          </svg>
+        </div>
+      </div>
       </div>
       <div className='col-span-6 border border-slate-300 bg-white rounded-md p-4'>
         <Outlet />
--- src/pages/Dashboard/EmployeeDate/EmployeeDate.tsx
+++ src/pages/Dashboard/EmployeeDate/EmployeeDate.tsx
@@ -1,66 +1,114 @@
-import React, { useState } from 'react'
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  Pagination,
-  getKeyValue
-} from '@nextui-org/react'
-import { users } from './data'
-
-export default function EmployeeDate() {
-  const [page, setPage] = useState(1)
-  const rowsPerPage = 8
-
-  const pages = Math.ceil(users.length / rowsPerPage)
-
-  const items = React.useMemo(() => {
-    const start = (page - 1) * rowsPerPage
-    const end = start + rowsPerPage
-
-    return users.slice(start, end)
-  }, [page, users])
+export default function EmployeeData() {
+  const employeeList = [
+    {
+      id: 1,
+      name: 'Alice',
+      shift: 'Morning',
+      time: '8:00 AM - 1:00 PM',
+      status: 'Active'
+    },
+    {
+      id: 2,
+      name: 'Bob',
+      shift: 'Afternoon',
+      time: '1:00 PM - 6:00 PM',
+      status: 'On Break'
+    },
+    {
+      id: 3,
+      name: 'Charlie',
+      shift: 'Evening',
+      time: '6:00 PM - 10:00 PM',
+      status: 'Available'
+    },
+    {
+      id: 1,
+      name: 'Alice',
+      shift: 'Morning',
+      time: '8:00 AM - 1:00 PM',
+      status: 'Active'
+    },
+    {
+      id: 2,
+      name: 'Bob',
+      shift: 'Afternoon',
+      time: '1:00 PM - 6:00 PM',
+      status: 'On Break'
+    },
+    {
+      id: 3,
+      name: 'Charlie',
+      shift: 'Evening',
+      time: '6:00 PM - 10:00 PM',
+      status: 'Available'
+    },
+    {
+      id: 1,
+      name: 'Alice',
+      shift: 'Morning',
+      time: '8:00 AM - 1:00 PM',
+      status: 'Active'
+    },
+    {
+      id: 2,
+      name: 'Bob',
+      shift: 'Afternoon',
+      time: '1:00 PM - 6:00 PM',
+      status: 'On Break'
+    }
+  ]
 
   return (
-    <Table
-      aria-label='Example table with client side pagination'
-      bottomContent={
-        <div className='flex w-full justify-center'>
-          <Pagination
-            isCompact
-            showControls
-            showShadow
-            color='primary'
-            page={page}
-            total={pages}
-            onChange={(page) => setPage(page)}
-          />
+    <div className='p-2 bg-white rounded-lg mt-6 shadow-md mx-auto min-h-[420px]'>
+      <div className='grid grid-cols-12 bg-gray-400/80 text-white my-2 text-lg font-medium p-4 py-2 rounded-xl text-center'>
+        <div className='col-span-2 text-left'>Employee ID</div>
+        <div className='col-span-3 text-left'>Full Name</div>
+        <div className='col-span-2 text-left'>Shift</div>
+        <div className='col-span-3 text-left'>Time</div>
+        <div className='col-span-2 text-left'>Status</div>
+      </div>
+      {employeeList.map((employee) => (
+        <div key={employee.id} className='bg-white h-[30px] mb-1 px-4 grid grid-cols-12 text-center items-center'>
+          <div className='col-span-2 text-left'>{employee.id}</div>
+          <div className='col-span-3 text-left'>{employee.name}</div>
+          <div className='col-span-2 text-left'>{employee.shift}</div>
+          <div className='col-span-3 text-left'>{employee.time}</div>
+          <div className='col-span-2 text-left'>{employee.status}</div>
         </div>
-      }
-      fullWidth
-      classNames={{
-        wrapper: 'min-h-[420px]'
-      }}
-    >
-      <TableHeader className='grid grid-cols-5 grid-rows-5'>
-        <TableColumn align='center' key='name'>
-          NAME
-        </TableColumn>
-        <TableColumn align='center' key='role'>
-          <div className='flex justify-start'>ROLE</div>
-        </TableColumn>
-        <TableColumn align='center' key='status'>
-          STATUS
-        </TableColumn>
-      </TableHeader>
-      <TableBody items={items} className='pb-0!'>
-        {(item) => (
-          <TableRow key={item.name}>{(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}</TableRow>
-        )}
-      </TableBody>
-    </Table>
+      ))}
+      {/* {employeeList && (
+          <div className='p-1'>
+            <div className='text-lg rounded-lg min-h-[310px] font-normal leading-none  text-blue-800 bg-blue-200 animate-pulse dark:bg-blue-900 dark:text-blue-200 text-center flex justify-center items-center'>
+              loading...
+            </div>
+          </div>
+        )} */}
+      <div className='flex justify-center mt-5'>
+        <div className='bg-slate-200/90 px-5 rounded-md mr-1 flex items-center transition'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='white'
+            className='size-7'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18' />
+          </svg>
+        </div>
+        <div className='bg-slate-200/90 px-5 rounded-md ml-1 flex items-center transition'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='white'
+            className='size-7'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3' />
+          </svg>
+        </div>
+      </div>
+    </div>
   )
 }

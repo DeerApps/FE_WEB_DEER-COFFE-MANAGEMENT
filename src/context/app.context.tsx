88a4f7a328cb1b653d5eb1def546f959f3582fca
bentran1vn <tan182205@gmail.com>
2024-06-16 11:06:03 +0700

    Authorization with token

--- src/context/app.context.tsx
+++ src/context/app.context.tsx
@@ -1,24 +1,21 @@
 import { createContext, useState } from 'react'
-import { Employee } from 'src/types/employee.type'
-import { getAccessTokenToLS, getProfileFromLS } from 'src/utils/auth'
+import { getAccessTokenToLS } from 'src/utils/auth'
 import { jwtDecode } from 'jwt-decode'
 import { CustomJwtPayload } from 'src/pages/Login/Login'
 
 interface AppContextInterface {
   isAuthenticated: boolean
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
-  employee: Employee | null
-  setEmployee: React.Dispatch<React.SetStateAction<Employee | null>>
   user: CustomJwtPayload | null
+  setUser: React.Dispatch<React.SetStateAction<CustomJwtPayload | null>>
   reset: () => void
 }
 
 const intialAppContext: AppContextInterface = {
   isAuthenticated: Boolean(getAccessTokenToLS()),
   setIsAuthenticated: () => null,
-  employee: getProfileFromLS(),
-  setEmployee: () => null,
   user: getAccessTokenToLS() !== '' ? jwtDecode<CustomJwtPayload>(getAccessTokenToLS()) : null,
+  setUser: () => null,
   reset: () => null
 }
 
@@ -26,16 +23,15 @@ export const AppContext = createContext<AppContextInterface>(intialAppContext)
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(intialAppContext.isAuthenticated)
-  const [employee, setEmployee] = useState<Employee | null>(intialAppContext.employee)
-  const [user] = useState<CustomJwtPayload | null>(intialAppContext.user)
+  const [user, setUser] = useState<CustomJwtPayload | null>(intialAppContext.user)
 
   const reset = () => {
     setIsAuthenticated(false)
-    setEmployee(null)
+    setUser(null)
   }
 
   return (
-    <AppContext.Provider value={{ isAuthenticated, setIsAuthenticated, employee, setEmployee, reset, user }}>
+    <AppContext.Provider value={{ isAuthenticated, setIsAuthenticated, reset, user, setUser }}>
       {children}
     </AppContext.Provider>
   )

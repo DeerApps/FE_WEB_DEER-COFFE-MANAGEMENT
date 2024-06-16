d1899c676ed926d6d694e58063bf492695911da9
bentran1vn <tan182205@gmail.com>
2024-06-16 15:01:12 +0700

    Fix Context Error

--- src/context/app.context.tsx
+++ src/context/app.context.tsx
@@ -1,13 +1,16 @@
 import { createContext, useState } from 'react'
-import { getAccessTokenToLS } from 'src/utils/auth'
+import { getAccessTokenToLS, getProfileFromLS } from 'src/utils/auth'
 import { jwtDecode } from 'jwt-decode'
 import { CustomJwtPayload } from 'src/pages/Login/Login'
+import { Employee } from 'src/types/employee.type'
 
 interface AppContextInterface {
   isAuthenticated: boolean
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
   user: CustomJwtPayload | null
   setUser: React.Dispatch<React.SetStateAction<CustomJwtPayload | null>>
+  employee: Employee | null
+  setEmployee: React.Dispatch<React.SetStateAction<Employee | null>>
   reset: () => void
 }
 
@@ -16,6 +19,8 @@ const intialAppContext: AppContextInterface = {
   setIsAuthenticated: () => null,
   user: getAccessTokenToLS() !== '' ? jwtDecode<CustomJwtPayload>(getAccessTokenToLS()) : null,
   setUser: () => null,
+  employee: getProfileFromLS(),
+  setEmployee: () => null,
   reset: () => null
 }
 
@@ -24,6 +29,7 @@ export const AppContext = createContext<AppContextInterface>(intialAppContext)
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(intialAppContext.isAuthenticated)
   const [user, setUser] = useState<CustomJwtPayload | null>(intialAppContext.user)
+  const [employee, setEmployee] = useState<Employee | null>(intialAppContext.employee)
 
   const reset = () => {
     setIsAuthenticated(false)
@@ -31,7 +37,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <AppContext.Provider value={{ isAuthenticated, setIsAuthenticated, reset, user, setUser }}>
+    <AppContext.Provider value={{ isAuthenticated, setIsAuthenticated, reset, user, setUser, employee, setEmployee }}>
       {children}
     </AppContext.Provider>
   )

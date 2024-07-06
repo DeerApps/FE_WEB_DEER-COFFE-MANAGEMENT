49227dc2467f965b8ad0af8d131db2fcb5319521
bentran1vn <tan182205@gmail.com>
2024-07-07 01:49:40 +0700

    Fix TypeScript Error in Building

--- src/pages/Apply/ApplyAbsentForm/AbsentForm.tsx
+++ src/pages/Apply/ApplyAbsentForm/AbsentForm.tsx
@@ -1,28 +1,27 @@
 import { Card } from '@nextui-org/react'
-import { Tabs, type TabsProps } from 'antd';
-import AbsentForms from './AbsentForms';
-import AbsentStatus from './AbsentStatus';
+import { Tabs, type TabsProps } from 'antd'
+import AbsentForms from './AbsentForms'
+import AbsentStatus from './AbsentStatus'
 
 export default function AbsentForm() {
-
   const items: TabsProps['items'] = [
     {
       key: '1',
       label: 'Absent Form',
-      children: <AbsentForms/>,
+      children: <AbsentForms />
     },
     {
       key: '2',
       label: 'Status',
-      children: <AbsentStatus/>,
-    },
+      children: <AbsentStatus />
+    }
   ]
 
   return (
     <>
-    <Card aria-label='card profile' className='max-w-6xl mx-auto p-4 shadow-none'>
-    <Tabs defaultActiveKey="1" items={items} />
-    </Card>
-  </>
+      <Card aria-label='card profile' className='max-w-6xl mx-auto p-4 shadow-none'>
+        <Tabs defaultActiveKey='1' items={items} />
+      </Card>
+    </>
   )
 }
